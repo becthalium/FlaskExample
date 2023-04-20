0cfeca6cda2df13d4b4413dbc1776a0a63296a91
becthalium <130136197+becthalium@users.noreply.github.com>
2023-04-20 11:21:21 +0300

    Update index.py

--- python/flask/api/index.py
+++ python/flask/api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+from flask import Flask, request
+import telebot
+app =Flask(__name__ ) 
+url="tewvtew"
+bot = telebot.TeleBot("6244126708:AAF-8_qFjbs2tuyTwZQ9iVpAduLnBX6t_iE")
+bot.remove_webhook()
+bot.set_webhook(url=url)
+@app.route('/'+secret, methods=['POST'])
+def webhook():
+      update=telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
+      tbot.bot.process_new_updates ([update])
+      return 'ok', 200
+      
+@bot.message_handler(commands=['start']):
+	def handle_start(message):
+		bot.reply_to(message.from_user.id, "Welcome to the ChatGPT Bot! You have 10 trial messages to use. After that, you'll need to pay for continued usage.")
+bot.run()
